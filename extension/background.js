76230e848d524e07f44659a1d8d72d4b806aa8ad
hello-worldyj <sis00011086@gmail.com>
2026-01-17 23:45:05 +0700

    Update background.js

--- extension/background.js
+++ extension/background.js
@@ -1,9 +1,7 @@
 chrome.commands.onCommand.addListener((command) => {
-  if (command === "solve-screen") {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const tab = tabs[0];
-      if (!tab?.id) return;
-
+  if (command === "solve") {
+    chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+      if (!tab) return;
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
         files: ["content.js"]
